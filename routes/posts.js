9e13d949305ca5d43f331daf4820c9665a9df77f
bobbobdude <thomasrobertgardner1056@gmail.com>
2024-02-15 17:37:31 +0100

    PHASE A: SET UP YOUR MINIWALL PROJECT AND INSTALL NECESSARY LIBRARIES is complete now as I have implemented a RESTful API and set up all the node.js libraries.

--- routes/posts.js
+++ routes/posts.js
@@ -3,6 +3,7 @@ const router = express.Router()
 
 const Post = require('../models/Post')
 
+//GET all users
 router.get("/", async (req, res) =>{
     try{
         const posts = await Post.find()
@@ -12,6 +13,7 @@ router.get("/", async (req, res) =>{
     }
 })
 
+//GET individual user 
 router.get('/:userId', async(req,res)=>{
     try{
         const specificPostById = await Post.findById(req.params.userId)
@@ -21,8 +23,8 @@ router.get('/:userId', async(req,res)=>{
     }
 })
 
+//POST data to database
 router.post('/', async(req,res)=>{
-    //console.log(req.body)
     const postData = new Post({
         post_title: req.body.post_title,
         timestamp: req.body.timestamp,
@@ -38,6 +40,36 @@ router.post('/', async(req,res)=>{
         res.send({message:err})
     }
 
+})
+
+//PATCH existing data
+router.patch('/:postId', async(req,res) =>{
+    try{
+        const updatePostById = await Post.updateOne(
+            {_id:req.params.postId},
+            {$set:{
+                post_title: req.body.post_title,
+                timestamp: req.body.timestamp,
+                post_owner: req.body.post_owner,
+                post_description: req.body.post_description,
+                likes: req.body.likes
+                }
+            })
+        res.send(updatePostById)
+    }catch(err){
+        res.send({message:err})
+    }
+})
+
+//DELETE specific user 
+router.delete('/:postId', async(req,res)=>{
+    try{
+        const deletePostById = await Post.deleteOne({_id:req.params.postId})
+        res.send(deletePostById)
+
+    }catch(err){
+        res.send({message:err})
+    }
 })
 
 module.exports = router 
