6040583b96306de7843ca3b4a04b64c559e53900
bobbobdude <thomasrobertgardner1056@gmail.com>
2024-03-04 12:05:39 +0100

    Fixed error in code where I try to send two responses due to an error - added return statement to first if error section.

--- routes/auth.js
+++ routes/auth.js
@@ -7,10 +7,23 @@ const {registerValidation} = require('../validations/validation')
 router.post('/register', async(req,res)=>{
     const {error} = registerValidation(req.body)
     if(error){
-        res.send({message:error['details'][0]['message']})
+        return res.status(400).send({message:error['details'][0]['message']})
     }
 
-    
+    const user = new User({
+        username:req.body.username,
+        email:req.body.email,
+        password:req.body.password
+
+    })
+
+    try{
+        const savedUser = await user.save()
+        res.send(savedUser)
+    }catch(err){
+        res.status(400).send({message:err})
+    }
+
 })
 
 router.post('/login', async(req,res)=>{
